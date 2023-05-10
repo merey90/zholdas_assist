4bc79a43df3dd5299e557a92e70cc2f188972f18
Merey <merey.90@gmail.com>
2023-05-10 20:00:48 +0200

    fix ts issues

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,31 @@
-addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request))
-})
+import { ITelegramResponse } from './types';
+declare const TELEGRAM_API_KEY: string;
 
-async function handleRequest(request) {
-  if (request.method === "POST") {
-    const payload = await request.json() 
-    // Getting the POST request JSON payload
-    if ('message' in payload) { 
-      // Checking if the payload comes from Telegram
-      const chatId = payload.message.chat.id
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request));
+});
 
-      if(payload.message.chat.username !== "merey90") {
-        const text = "Sorry, you are not allowed to use my 🤖"
-        const url = `https://api.telegram.org/bot${TELEGRAM_API_KEY}/sendMessage?chat_id=${chatId}&text=${text}`
-        const data = await fetch(url).then(resp => resp.json()) 
-      }
-      else {
-        const text = payload.message.text + " over " + payload.message.chat.username
-        const url = `https://api.telegram.org/bot${TELEGRAM_API_KEY}/sendMessage?chat_id=${chatId}&text=${text}`
-        const data = await fetch(url).then(resp => resp.json()) 
+export interface Env {
+  TELEGRAM_API_KEY: string;
+}
+
+async function handleRequest(request: Request<unknown, CfProperties<unknown>>) {
+  if (request.method === 'POST') {
+    const payload = await request.json<ITelegramResponse>();
+    if ('message' in payload) {
+      const chatId = payload.message.chat.id;
+
+      if (payload.message.chat.username !== 'merey90') {
+        const text = 'Sorry, you are not allowed to use my 🤖';
+        const url = `https://api.telegram.org/bot${TELEGRAM_API_KEY}/sendMessage?chat_id=${chatId}&text=${text}`;
+        const data = await fetch(url).then((resp) => resp.json());
+      } else {
+        const text =
+          payload.message.text + ' over ' + payload.message.chat.username;
+        const url = `https://api.telegram.org/bot${TELEGRAM_API_KEY}/sendMessage?chat_id=${chatId}&text=${text}`;
+        const data = await fetch(url).then((resp) => resp.json());
       }
     }
   }
-  return new Response("OK") // Doesn't really matter
+  return new Response('OK');
 }
---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export interface ITelegramResponse {
+  update_id: number;
+  message: {
+    message_id: number;
+    from: {
+      id: string;
+      is_bot: false;
+      first_name: string;
+      username: string;
+      language_code: string;
+    };
+    chat: {
+      id: string;
+      first_name: string;
+      username: string;
+      type: string;
+    };
+    date: number;
+    text: string;
+  };
+}
